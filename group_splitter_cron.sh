2f7cac31862b73349e2fa2283f81f2473a05aa31
Filip Makosza <fm12@sanger.ac.uk>
2020-03-04 10:10:47 +0000

    Fixed wrong cron/manager script arguments

--- group_splitter_cron.sh
+++ group_splitter_cron.sh
@@ -2,4 +2,4 @@
 source /usr/local/lsf/conf/profile.lsf
 declare REPORT_DIR="/lustre/scratch115/teams/hgi/lustre-usage/"
 
-bsub -o "${REPORT_DIR}/group-splitter-logs/$(date '+%Y-%m-%d').%J.out" -G hgi -R "select[mem>1000] rusage[mem=1000] span[hosts=1]" -M 1000 -n 1 "${REPORT_DIR}group_splitter_manager.sh -o /lustre/scratch115/teams/hgi/lustre-usage/groups/"
+bsub -o "${REPORT_DIR}/group-splitter-logs/$(date '+%Y-%m-%d').%J.out" -G hgi -R "select[mem>1000] rusage[mem=1000] span[hosts=1]" -M 1000 -n 1 "${REPORT_DIR}group_splitter_manager.sh"
--- group_splitter_manager.sh
+++ group_splitter_manager.sh
@@ -3,6 +3,5 @@
 declare REPORT_DIR="/lustre/scratch115/teams/hgi/lustre-usage/"
 declare PY_ENV="/lustre/scratch115/teams/hgi/lustre-usage/.lurge_env/bin/"
 
-cd "${REPORT_DIR}"
-"${PY_ENV}"python3 "${REPORT_DIR}"group_splitter.py
+"${PY_ENV}"python3 "${REPORT_DIR}"group_splitter.py -o "${REPORT_DIR}/groups/"
 s3cmd sync "${REPORT_DIR}groups/" "s3://branchserve/mpistat/"
