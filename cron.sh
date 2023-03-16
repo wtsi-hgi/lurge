30adc99a1da727fd1d01e0411423a31bf021581e
Altaf Ali <7231912+altaf-ali@users.noreply.github.com>
2023-03-16 15:04:27 +0000

    add lustre volumes 125 and 126 #9

--- cron.sh
+++ cron.sh
@@ -7,7 +7,7 @@ INSTANCE="dev"
 export INSTANCE
 
 SOFTWARE_ROOT="/software/hgi/installs/lurge"
-export REPORT_DIR="/lustre/scratch119/humgen/teams/hgi/lurge/$INSTANCE"
+export REPORT_DIR="/lustre/scratch126/humgen/teams/hgi/lurge/$INSTANCE"
 
 run_splitter=""
 [[ $INSTANCE == "prod" ]] && run_splitter="splitter"
@@ -22,8 +22,8 @@ bsub \
 # Run main reporter with MPI
 # NUM_CPUs is 1 + number of volumes * (WORKERS_PER_VOLUME + 1)
 # WORKERS_PER_VOLUME is defined towards top of group_reporter.py
-# i.e. 5 volumes (117, 118, 119, 123, 124), 6 workers per volume = 36 cores 
-NUM_CPUs=36
+# i.e. 5 volumes (117, 118, 119, 123, 124, 125, 126), 6 workers per volume = 50 cores
+NUM_CPUs=50
 
 export LD_LIBRARY_PATH=/software/openmpi-4.0.3/lib:$LD_LIBRARY_PATH
 export PATH=/software/openmpi-4.0.3/bin:$PATH
@@ -32,7 +32,7 @@ bsub \
     -o $REPORT_DIR/report-logs/$(date '+%Y-%m-%d').%J.group_report.out \
     -e $REPORT_DIR/report-logs/$(date '+%Y-%m-%d').%J.group_report.err \
     -G hgi \
-    -R "select[mem>3000] rusage[mem=3000]" -M 3000 -n $NUM_CPUs \
+    -R "select[mem>5000] rusage[mem=5000]" -M 5000 -n $NUM_CPUs \
     "mpirun $SOFTWARE_ROOT/.venv/bin/python3 $SOFTWARE_ROOT/$INSTANCE/group_reporter.py"
     
 
